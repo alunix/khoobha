f408f086c5dbdc5ff3179c199b0a866a6ff31e78
Alireza Nourian <alireza.nournia@gmail.com>
2014-06-26 14:45:29 +0430

    fix window request feature

--- app/src/main/java/ir/sobhe/khoobha/MainActivity.java
+++ app/src/main/java/ir/sobhe/khoobha/MainActivity.java
@@ -46,9 +46,9 @@ public class MainActivity extends android.app.Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_main);
         getActionBar().setTitle("خوب‌ها");
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 
         dataSource = new ActivityDataSource(this);
         dataSource.open();
