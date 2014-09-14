0fb3ac48d4950677d28cea4b9c03abeee89abfa3
Alireza Nourian <az.nourian@gmail.com>
2014-09-14 18:06:20 +0430

    add quran activities

--- app/src/main/java/ir/sobhe/khoobha/DatabaseHelper.java
+++ app/src/main/java/ir/sobhe/khoobha/DatabaseHelper.java
@@ -1,19 +1,14 @@
 package ir.sobhe.khoobha;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.provider.MediaStore;
-import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.sql.SQLInput;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
